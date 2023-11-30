03584bfc8c718847c678515c1c650598199aa77e
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-11-30 17:28:58 +0700

    fix: Add page max width

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,20 @@
 import "./header.scss";
 
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="header">
       <div className="header__wrapper">
-        <h1 className="logo" >Logo</h1>
+        <div className="logo" >Logo</div>
         <div className="navbar">
             <a href="">Latest</a>
             <a href="">Our Story</a>
             <a href="">Write</a>
         </div>
-        <a className="account" href="">Log in</a>
+        <Link className="account" href='#'>
+          Log in
+        </Link>
       </div>
     </header>
   );

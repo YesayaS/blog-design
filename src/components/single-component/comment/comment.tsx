da2904912a9d9ba1e9d056637863d1f9772792f4
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-12-01 21:12:12 +0700

    feat: Add comment component

---
+++ src/components/single-component/comment/comment.tsx
@@ -0,0 +1,16 @@
+import { IoEllipsisVertical } from "react-icons/io5";
+
+import './comment.scss'
+
+export default function Comment() {
+    return (
+        <div className='comment'>
+            <div className="comment__detail">
+                <div className="comment__author">author</div>
+                <div className="comment__date">2 Nov 23</div>
+                <div className="comment__content">Much wow</div>
+            </div>
+            <div className="comment__setting"><button><IoEllipsisVertical /></button></div>
+        </div>
+    )
+}

d5cfdb32d13399a2db902121afcabb31b8334840
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-12-01 21:22:31 +0700

    refactor: Padding layout

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -2,9 +2,11 @@ import ArticlePost from "./post";
 import CommentSection from "./commentSection";
 import Divider from "@/src/components/collections/lineDivider/lineDivider";
 
+import './page.scss'
+
 export default function Post() {
   return (
-    <div>
+    <div className="article-post">
       <ArticlePost />
       <Divider />
       <CommentSection/>

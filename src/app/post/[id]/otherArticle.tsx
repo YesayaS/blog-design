1eb060014e58ec8ffb538e518e1d8b1d8bb20b17
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-12-01 21:32:43 +0700

    feat: Add other article section

---
+++ src/app/post/[id]/otherArticle.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/src/components/single-component/hero/hero";
+
+import './otherArticle.scss'
+
+export default function OtherArticle() {
+    return (
+        <div className="o-article" id="other-article" >
+            <div className="o-article__header">Other Article</div>
+            <div className="o-article__heros">
+                <Hero/>
+                <Hero/>
+            </div>
+        </div>
+    )
+}

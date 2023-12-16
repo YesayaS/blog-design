7bee5f760bd168baf8c58955e2a0d4caf328b159
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-12-16 17:22:32 +0700

    feat: Add home latest posts

--- src/components/collections/latest/latest.tsx
+++ src/components/collections/latest/latest.tsx
@@ -1,18 +1,44 @@
+"use client";
+
 import Link from "next/link";
 
 import CardList from "@/src/components/single-component/cardList/cardList";
 
 import "./latest.scss";
+import { useEffect, useState } from "react";
+import fetchAPI from "@/src/utils/fetchAPI";
 
 export default function Latest() {
+  const [posts, setPosts] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("" as any);
+
+  useEffect(() => {
+    const options = { method: "GET", "Content-Type": "application/json" };
+    const fetchPost = async () => {
+      const { response, error } = await fetchAPI(`/posts`, options);
+      if (error) {
+        setError(error);
+        setLoading(false);
+      }
+
+      if (response) {
+        setPosts(response.posts);
+        setLoading(false);
+      }
+    };
+    fetchPost();
+  }, []);
+
+  if (!posts) return <p>load</p>;
+
   return (
     <div className="latest">
       <h1 className="latest__title">Latest</h1>
       <div className="latest__card-list">
-        <CardList />
-        <CardList />
-        <CardList />
-        <CardList />
+        {posts.map((post: any, i: number) => {
+          return <CardList key={i} postData={post} />;
+        })}
       </div>
       <Link href="/">
         <button className="button__see-more button-hover">
--- src/components/single-component/cardList/cardList.tsx
+++ src/components/single-component/cardList/cardList.tsx
@@ -2,29 +2,38 @@ import Image from "next/image";
 import Link from "next/link";
 
 import "./cardList.scss";
+import { decode } from "@/src/utils/utils";
 
-export default function CardList() {
+export default function CardList({ postData }: { postData: any }) {
+  const { _id, title, sub_title, title_img, publication_date, author } =
+    postData;
+
+  console.log(_id, title, sub_title, title_img, publication_date, author);
+
+  const isValidImageUrl =
+    title_img && /^(ftp|http|https):\/\/[^ "]+$/.test(decode(title_img));
+
   return (
-    <Link href="/post/1">
+    <Link href={`/post/${_id}`}>
       <div className="card-list">
-        <div className="card-list__img">
-          <Image
-            src="https://images.pexels.com/photos/323772/pexels-photo-323772.jpeg?auto=compress&cs=tinysrgb&h=350"
-            alt="Picture of a house"
-            fill
-            style={{ objectFit: "cover" }}
-            sizes="100vw, (min-width: 768px) 33vw"
-          />
-        </div>
+        {isValidImageUrl && (
+          <div className="card-list__img">
+            <Image
+              src={decode(title_img)}
+              alt=""
+              fill
+              style={{ objectFit: "cover" }}
+              sizes="100vw, (min-width: 768px) 33vw"
+            />
+          </div>
+        )}
         <div className="card-list__detail">
-          <p className="card-list__detail__author">Author Name</p>
-          <p className="card-list__detail__header truncate">
-            Article Heading : Lorem Ipsum Lorem Ipsum Lorem Ipsum
-          </p>
+          <p className="card-list__detail__author">{decode(author.username)}</p>
+          <p className="card-list__detail__header truncate">{decode(title)}</p>
           <p className="card-list__detail__sub-header truncate">
-            Article Sub Heading : Lorem Ipsum Lorem Ipsum Lorem Ipsum{" "}
+            {decode(sub_title)}
           </p>
-          <p className="card-list__detail__publish-date">2 Nov 23</p>
+          <p className="card-list__detail__publish-date">{publication_date}</p>
         </div>
       </div>
     </Link>
